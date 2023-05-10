50d2979b62084324bba6bcca7f8022fb2f44d42d
Thiago Alves <thiagoalves@ioasys.com.br>
2023-05-10 18:26:14 -0300

    4.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.library").version("8.1.0-alpha11").apply(false)
     kotlin("multiplatform").version("1.8.10").apply(false)
-    id("com.jfrog.artifactory").version ("4.29.1").apply(false)
     id("maven-publish")
 }
 
@@ -12,7 +11,6 @@ tasks.register("clean", Delete::class) {
 
 subprojects {
     apply(plugin = "maven-publish")
-    apply(plugin = "com.jfrog.artifactory")
 
     afterEvaluate {
         plugins.withId("com.android.library") {
@@ -20,7 +18,7 @@ subprojects {
                 publications {
                     register<MavenPublication>("release") {
                         groupId = "com.github.thigott"
-                        version = "4.0.0"
+                        version = "1.0.0"
 
                         afterEvaluate {
                             from(components["release"])
--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.library")
 }
 
-version = "4.0.0"
+version = "1.0.0"
 
 kotlin {
     android {
